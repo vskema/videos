04727496f9dfbeedb1d8ff55e9a0cabd7d9e8c01
Vaidotas <skema.vaidotas@gmail.com>
2020-08-19 14:03:19 +0300

    add video player

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from "../apis/youtube";
 
 const KEY = 'AIzaSyDEoSD0LSlLSb_RO-tNQCvOxBXe9hjHBRs';
 
 class App extends React.Component {
 
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async term => {
         const responce = await youtube.get('/search', {
@@ -23,13 +24,22 @@ class App extends React.Component {
         this.setState({videos: responce.data.items});
     };
 
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video})
+    }
+
 
     render() {
         return (
             <div className= "ui container">
                 <div>
                     <SearchBar onFormSubmit={this.onTermSubmit} />
-                    <VideoList videos={this.state.videos} />
+                    <VideoDetail video={this.state.selectedVideo} />
+                    <VideoList
+                        videos={this.state.videos}
+                        onVideoSelect = {this.onVideoSelect}
+                    />
+
                 </div>
             </div>
         );
